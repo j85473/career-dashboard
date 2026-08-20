57cff224c569a94dcc291f1796fa5b465ade83c2
j85473 <294842758+j85473@users.noreply.github.com>
2026-08-19 21:07:39 -0500

    Remove obsolete DuckDuckGo scratch probe

--- scratch_ddg.ts
+++
@@ -1,7 +0,0 @@
-import { search, SafeSearchType } from 'duck-duck-scrape';
-async function run() {
-  const query = 'site:adzuna.com/details/ "sales" "Minnesota"';
-  const results = await search(query, { safeSearch: SafeSearchType.OFF });
-  console.log(results.results.slice(0, 3));
-}
-run().catch(console.error);
